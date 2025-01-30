a8bfa7e43cdcd6fdde1f797e1bc54cd1449413bb
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-01-30 20:24:49 +0100

    feat: transfer component properties by props

--- app.jsx
+++ app.jsx
@@ -3,21 +3,24 @@ const rootEl = document.getElementById('root')
 const root = ReactDOM.createRoot(rootEl)
 
 //creare una componente React
-const App = () =>{
+const App = (props) =>{
     return <main className="main">
         questa è una componente React
+        {props.children}
     </main>
 
 }
 
 root.render( // scrivere in html, poi convertito in javaScript da babel
     <>
-        <App></App> 
-        <h1>Using JSX</h1>
-        <ul>
-            <li>PHP</li>
-            <li>JavaScript</li>
-            <li>HTML</li>
-        </ul>
+        <App>
+            <h1>Using JSX</h1>
+            <ul>
+                <li>PHP</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+            </ul>
+        </App> 
+
     </>
 )
