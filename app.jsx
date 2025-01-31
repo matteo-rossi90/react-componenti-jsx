96afb23bd603402616aafef5c07e8eced9e096d5
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-01-31 20:14:15 +0100

    feat: add new component

--- app.jsx
+++ app.jsx
@@ -3,23 +3,28 @@ const rootEl = document.getElementById('root')
 const root = ReactDOM.createRoot(rootEl)
 
 //creare una componente React
-const App = (props) =>{
+const App = ({children}) =>{ //props passa le informazioni e le proprietà dei figli alla componente, oppure si usa children
     return <main className="main">
         questa è una componente React
-        {props.children}
+        {children}
     </main>
 
 }
+
+function List() { // altra componente
+    return <ul>
+                <li>PHP</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+        </ul >
+};
+
 
 root.render( // scrivere in html, poi convertito in javaScript da babel
     <>
         <App>
             <h1>Using JSX</h1>
-            <ul>
-                <li>PHP</li>
-                <li>JavaScript</li>
-                <li>HTML</li>
-            </ul>
+            <List/> 
         </App> 
 
     </>
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 Creare componenti con React
 
+Si tratta di un primo approccio alla creazione dei componenti con React usando JSX, per capirrne soprattutto la sintassi e prendere dimestichezza con la logica e i vari modi con cui si possono creare le componenti mettendo in risalto il rapporto tra elementi parent ed elementi children.
